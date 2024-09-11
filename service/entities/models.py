e664290b2a37ca063732bbceac087e33fa0cbd90
Grishaak <antonianogroboracevskij@gmail.com>
2024-09-11 20:34:39 +0300

    Rename model in clients

--- service/entities/models.py
+++ service/entities/models.py
@@ -1,20 +1,23 @@
-from django.db import models
-
-class Service(models.Model):
-    name = model.CharField(max_length=50)
-    full_price = model.PositiveIntegerField()
-    
-
-
-class Plan(models.Model):
-    PLAN_TYPES = (
-	("full", "Full"), ("student", "Student"), ("dicount","Discount")
-)
-    plan_type = model.CharField(choices=PLAN_TYPES, max_length=10)
-    discount_percent = model.PositiveIntegerField(default=0,
-						  validators=[
-						      MaxValueValidator(100)
-						  ])
-
-
-
+# from django.core.validators import MaxValueValidator
+# from django.db import models
+# from service.clients.models import Client
+#
+# class Service(models.Model):
+#     name = models.CharField(max_length=50)
+#     full_price = models.PositiveIntegerField()
+#
+#
+# class Plan(models.Model):
+#     PLAN_TYPES = (
+#         ("full", "Full"), ("student", "Student"), ("dicount", "Discount")
+#     )
+#     plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
+#     discount_percent = models.PositiveIntegerField(default=0,
+#                                                    validators=[
+#                                                        MaxValueValidator(100)
+#                                                    ])
+#
+#
+# class Subscription(models.Model):
+#     client = models.ForeignKey(Client,
+#                                related_name="subscriptions")
