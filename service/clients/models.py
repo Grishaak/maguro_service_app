6e4f4abb38ef62b8bf2bd26931fb9a5ad9dbf0bb
Grishaak <antonianogroboracevskij@gmail.com>
2024-09-11 19:03:14 +0300

    commit it again

--- service/clients/models.py
+++ service/clients/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    company_name = models.CharField(max_length=100)
+    
