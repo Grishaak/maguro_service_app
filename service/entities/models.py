b3995a8829daba5e84b45f5c6cdc120e8daf09c5
Grishaak <antonianogroboracevskij@gmail.com>
2024-09-11 20:25:02 +0300

    ADded models to clients

--- service/entities/models.py
+++ service/entities/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    name = model.CharField(max_length=50)
+    full_price = model.PositiveIntegerField()
+    
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+	("full", "Full"), ("student", "Student"), ("dicount","Discount")
+)
+    plan_type = model.CharField(choices=PLAN_TYPES, max_length=10)
+    discount_percent = model.PositiveIntegerField(default=0,
+						  validators=[
+						      MaxValueValidator(100)
+						  ])
+
+
+
