417684f622894714350d4463bee3f4075d568f7f
Grishaak <antonianogroboracevskij@gmail.com>
2024-09-13 17:27:26 +0300

    commit for win system

--- service/entities/views.py
+++ service/entities/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from entities.models import Subscription
+from entities.serializers import SerializerSubscription
 
-# Create your views here.
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SerializerSubscription
