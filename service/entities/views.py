3ebbf1fad33469056a4ba0d40fb422f2c7058ad5
Grishaak <antonianogroboracevskij@gmail.com>
2024-09-13 21:16:50 +0300

    Added prefetch for ORM view endpoint

--- service/entities/views.py
+++ service/entities/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Prefetch
+
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from entities.models import Subscription
+
+from clients.models import Client
+from entities.models import Subscription, Plan
 from entities.serializers import SerializerSubscription
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch('client',
+            queryset=Client.objects.select_related('user').only('company_name', 'user__email')),
+        Prefetch('plan',
+            queryset=Plan.objects.only('plan_type')))
     serializer_class = SerializerSubscription
