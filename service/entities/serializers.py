3020375731aa242c716258a7c6ccb5392405071a
Grishaak <antonianogroboracevskij@gmail.com>
2024-09-13 18:40:14 +0300

    Changed plan_type

--- service/entities/serializers.py
+++ service/entities/serializers.py
@@ -5,7 +5,7 @@ from entities.models import Subscription
 class SerializerSubscription(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
-
+    plan_type = serializers.CharField(source='plan.plan_type')
     class Meta:
         model = Subscription
-        fields = ('pk', 'plan_plan_type', 'client_name', 'email')
+        fields = ('pk', 'plan_type', 'client_name', 'email')
