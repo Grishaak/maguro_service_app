96788c16faf79f9be67c7fde44abd655409d18fd
Grishaak <antonianogroboracevskij@gmail.com>
2024-09-13 17:49:19 +0300

    modified serailizer

--- service/entities/serializers.py
+++ service/entities/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 from entities.models import Subscription
 
+
 class SerializerSubscription(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
 
     class Meta:
         model = Subscription
-        fields = ('pk', 'plan_id','client_name', 'email')
-
+        fields = ('pk', 'plan_plan_type', 'client_name', 'email')
