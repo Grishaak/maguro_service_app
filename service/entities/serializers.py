38e98573ff8a38bb4235ea47fef9a62d9e785fad
Grishaak <antonianogroboracevskij@gmail.com>
2024-09-15 18:45:13 +0300

    refactored code in serializer

--- service/entities/serializers.py
+++ service/entities/serializers.py
@@ -2,16 +2,23 @@ from rest_framework import serializers
 from entities.models import Subscription, Plan
 
 
-
 class SerializerPlan(serializers.ModelSerializer):
     class Meta:
         model = Plan
-        fields =  ('__all__')
+        fields = ('__all__')
+
 
 class SerializerSubscription(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
     plan = SerializerPlan()
+    price = serializers.SerializerMethodField('get_price')
+
+    def get_price(self, instance: Subscription):
+        pre_price = instance.service.full_price
+        discount = pre_price * instance.plan.discount_percentage / 100
+        return pre_price - discount
+
     class Meta:
         model = Subscription
         fields = ('pk', 'plan', 'client_name', 'email')
--- service/entities/views.py
+++ service/entities/views.py
@@ -9,8 +9,12 @@ from entities.serializers import SerializerSubscription, SerializerPlan
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.select_related('plan').prefetch_related(Prefetch('client', queryset=Client.objects.select_related('user').only('company_name', 'user__email')))
+    queryset = (Subscription.objects.select_related('plan')
+                .prefetch_related
+                (Prefetch('client', queryset=Client.objects.select_related('user')
+                          .only('company_name', 'user__email'))))
     serializer_class = SerializerSubscription
+
 
 class PlanView(ReadOnlyModelViewSet):
     queryset = Plan.objects.all()
