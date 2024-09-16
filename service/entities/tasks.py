949788956a8f651ea41c4b456b87cf2cb97d1dfc
Grishaak <antonianogroboracevskij@gmail.com>
2024-09-17 00:25:20 +0300

    reworked tasks

--- service/entities/tasks.py
+++ service/entities/tasks.py
@@ -4,11 +4,10 @@ from celery import shared_task
 @shared_task
 def set_price(subscription_id):
     from entities.models import Subscription
-    
 
     subscription = Subscription.objects.get(id=subscription_id)
     new_price = subscription.service.full_price \
-            - (subscription.service.full_price *
-                    subscription.plan.disount_percent) / 100
+                - (subscription.service.full_price *
+                   subscription.plan.disount_percent) / 100
     subscription.price = new_price
     subscription.save(save_model=False)
